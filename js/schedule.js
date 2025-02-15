7aede28d48a99ac8cc613fdf09c0171d0520032e
KimS1Yong <sdragon0928@naver.com>
2025-02-15 19:44:11 +0900

    Faet: favicon og 적용

--- js/schedule.js
+++ js/schedule.js
@@ -96,11 +96,11 @@ async function onYouTubeIframeAPIReady() {
   console.log("유튜브 API 준비 완료");
 
   // 브이로그 로딩 필요하면 아래 주석 해제하기:)
-  // 유튜브 영상 링크 요청
+  // // 유튜브 영상 링크 요청
   // await requestVlogList(theme, region);
-  // 유튜브 영상 플레이어 추가
+  // // 유튜브 영상 플레이어 추가
   // appendYoutubeVlog();
-  // 모든 플레이어가 준비되면 스피너 제거
+  // // 모든 플레이어가 준비되면 스피너 제거
   // document.getElementById("spinner").remove();
 }
 
