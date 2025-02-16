9b86fb365db49f9053ffe7adc53a039f96f458c6
김주엽 <kjyy08@naver.com>
2025-02-16 18:41:52 +0900

    Fix: 일정 전체 복사 버튼 누르면 토스트 보이는 버그 수정

--- js/schedule.js
+++ js/schedule.js
@@ -137,15 +137,3 @@ prevControl.addEventListener("click", () => {
 nextControl.addEventListener("click", () => {
   stopYoutubePlayer();
 });
-
-// 복사 버튼 클릭 시 이벤트
-document
-  .querySelector(".ai-schedule-copy-button")
-  .addEventListener("click", function () {
-    // 버튼 클릭 시 복사 작업 (실제 복사 기능을 구현하고 싶다면 추가)
-
-    // Toast 생성 및 표시
-    var toastEl = document.getElementById("toastControl");
-    var toast = new bootstrap.Toast(toastEl);
-    toast.show();
-  });
